ee5d32c3bbd77673b23861c4ba3ef9d0513315b2
shanaka-mdosl <shanaka@mydigitaloffice.ca>
2021-09-27 18:49:34 +0530

    folder structure created for the model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End ML model for churn model ',
+    author='shanaka_chathuranga',
+    license='MIT',
+)
